d304879b487792afa5bb58cf7f966a89c6b2cd2a
Sebastian <sebos@protonmail.com>
2024-09-20 22:52:30 +0200

    feat: postgres + kysely setup

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { router } from './router/router';
+import { logger } from './logger';
+
+const app = express();
+
+app.use("/", router);
+
+app.use((req, _, next) => {
+  logger.info("New request received:", req.method, req.url);
+  next();
+});
+
+
+export { app }
+
