7339e5eea8def7d51408f4fb7902e0f6de600897
Sebastian <sebos@protonmail.com>
2024-09-24 00:13:05 +0200

    feat: frontend setup

---
+++ frontend/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+provideHeadlessUseId(() => useId())
+const dupa = ref('sranie')
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
