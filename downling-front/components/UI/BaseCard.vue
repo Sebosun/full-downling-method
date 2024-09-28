8e4fe803cc2d5016f7283996843c988ed7d34f08
Sebastian <sebos@protonmail.com>
2024-09-28 11:49:46 +0200

    feat: npm in frontend instead of pnpm which broke lsps

---
+++ downling-front/components/UI/BaseCard.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-gray-800 py-2 px-4 mb-4">
+        <slot />
+    </div>
+</template>
---
+++ downling-front/pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import BaseCard from '@/components/UI/BaseCard.vue'
+
+const dupa = useState()
+</script>
+
+<template>
+    <div>
+        <BaseCard>
+            <div>
+                dupa
+            </div>
+        </BaseCard>
+        <div>
+            dupa
+        </div>
+    </div>
+</template>
