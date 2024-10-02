dcf9c4ea5d719e6ead5bae464d31514508c4891a
Sebastian <sebos@protonmail.com>
2024-10-03 00:34:01 +0200

    feat: quality of life, keyboiard shortcuts for inputs of special characters

--- downling-front/components/base/BaseButton.vue
+++ downling-front/components/base/BaseButton.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  permaShadow?: boolean
+}>()
+</script>
 <template>
   <button
-    class="ui-button box-shadow-hover px-4 py-3 border border-solid border-black dark:border-white rounded-xs tracking-wide flex items-center justify-center text-center">
+    class="ui-button box-shadow-hover px-4 py-3 border border-solid border-black dark:border-white rounded-xs tracking-wide flex items-center justify-center text-center"
+    :class="{ 'ui-button-shadow': permaShadow }">
     <slot />
   </button>
 </template>
@@ -13,5 +19,9 @@
     transform: translate(-0.25rem, -0.25rem);
     box-shadow: .25rem .25rem 0rem #f85552;
   }
+}
+
+.ui-button-shadow {
+  box-shadow: .25rem .25rem 0rem #f85552;
 }
 </style>
--- downling-front/pages/index.vue
+++ downling-front/pages/index.vue
@@ -3,10 +3,14 @@ import type { Exercise } from '@/types/ExerciseTypes'
 const input = ref<string>('')
 const currentExercise = ref<Exercise | null>(null)
 const allExercises = ref<Exercise[] | null>(null)
+const showAnswer = ref<boolean>(false)
+
+const specialLatinLetters = ["ā", "ō", "ī", "ē"] as const
+const previousKeys = ref<string[]>([])
 
 const API_LINK = "http://localhost:3000"
 async function submit(): Promise<void> {
-  console.log('nothing')
+  showAnswer.value = false
 }
 
 async function getExercises(): Promise<void> {
@@ -20,21 +24,71 @@ onMounted(() => {
   getExercises()
 })
 
-const keyup = (event: unknown) => {
-  console.log(event)
+const onInput = (newInput: string) => {
+  input.value = newInput.trimEnd()
 }
 
-const specialLatinLetters = ["ā", "ō", "ī", "ē"]
+const preventSpace = (event: KeyboardEvent) => {
+  const forbiddenKeys = [" ", ","]
+  const isForbiddenKey = forbiddenKeys.includes(event.key)
+  if (isForbiddenKey) {
+    event.preventDefault()
+  }
+}
+
+const keyup = (event: KeyboardEvent) => {
+  // removing first element so we dont 
+  // store 45901923401923091203 number of itemms for no reason
+  if (previousKeys.value.length > 50) {
+    previousKeys.value.shift()
+  }
+  const len = previousKeys.value.length
+  const commandBefore = previousKeys.value[len - 1]
+
+  if (commandBefore === " " && commandBefore === event.key) {
+    showAnswer.value = true
+    previousKeys.value.pop()
+    return
+  }
+
+  previousKeys.value.push(event.key)
+
+  if (commandBefore === ",") {
+    const indexOfLastChar = input.value.length - 1
+    const newInputStrip = input.value.substring(0, indexOfLastChar)
+    if (event.key === "a") {
+      input.value = newInputStrip + specialLatinLetters[0]
+    }
+
+    if (event.key === "o") {
+      input.value = newInputStrip + specialLatinLetters[1]
+    }
+
+    if (event.key === "i") {
+      input.value = newInputStrip + specialLatinLetters[2]
+    }
+
+    if (event.key === "e") {
+      input.value = newInputStrip + specialLatinLetters[3]
+    }
+  }
+}
 </script>
 
 <template>
   <div>
     <BaseCard class="p-20">
-      <div>
-        <h1 class="text-center mb-8">{{ currentExercise?.question }}</h1>
+      <div class="text-center">
+        <h1 class="mb-4">{{ currentExercise?.question }}</h1>
+        <p class="my-4" v-if="showAnswer">{{ currentExercise?.answer }}</p>
         <form class="grid gap-4" @submit.prevent="submit">
-          <BaseInput @keyup.space="keyup" class="mx-auto max-w-[450px]" v-model:input="input"
-            placeholder="enter text here" />
+          <div class="flex mx-auto gap-4">
+            <BaseButton perma-shadow class="min-w-20" type="button" v-for="letter in specialLatinLetters" :key="letter">
+              {{ letter }}
+            </BaseButton>
+          </div>
+          <BaseInput @keydown="preventSpace" @keyup="keyup" class="mx-auto max-w-[450px]" :input="input"
+            @update:input="onInput" placeholder="enter text here" />
           <BaseButton class="mx-auto w-[450px] ">
             Do something
           </BaseButton>
