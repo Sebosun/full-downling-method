5ef49d1639f5312939f965c4534e593c10e9f73f
Sebastian <sebos@protonmail.com>
2025-02-17 22:11:44 +0100

    feat(backend): making questions unique

--- backend/src/helpers/jwt.ts
+++ backend/src/helpers/jwt.ts
@@ -2,11 +2,15 @@ import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET
 
+// Should crash app 
+// but thats on you since 
+// you are not setting any envs, dummy
 if (!JWT_SECRET) {
   throw new Error("JWT Secret not set")
 }
 
 export function signJWT(user_id: number): string {
+  if (!JWT_SECRET) throw new Error("JWT Secret not set")
   return jwt.sign({ userId: user_id }, JWT_SECRET, {
     expiresIn: '1h',
   });
--- backend/src/migrations/002.ts
+++ backend/src/migrations/002.ts
@@ -5,7 +5,7 @@ export async function up(db: Kysely<Database>): Promise<void> {
   await db.schema
     .createTable('exercises')
     .addColumn('id', 'serial', (col) => col.primaryKey())
-    .addColumn('question', 'varchar', (col) => col.notNull())
+    .addColumn('question', 'varchar', (col) => col.notNull().unique())
     .addColumn('answer', 'varchar', (col) => col.notNull())
     .addColumn('created_at', 'varchar', (col) =>
       col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull(),
