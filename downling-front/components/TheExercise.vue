a2f7bbc9bb461d65085e3030fbac728ec8b05ab8
Sebastian <sebos@protonmail.com>
2025-02-24 00:20:08 +0100

    feat(frontend): animations

--- downling-front/components/TheExercise.vue
+++ downling-front/components/TheExercise.vue
@@ -92,12 +92,18 @@ const keyup = async (event: KeyboardEvent) => {
     previousKeys.value.shift()
   }
   const len = previousKeys.value.length
-  const commandBefore = previousKeys.value[len - 1]
+  const lastLetter = previousKeys.value[len - 1]
 
-  if (commandBefore === ' ' && commandBefore === event.key) {
-    await store.fetchCorrectAnswer()
-    showAnswer.value = true
-    previousKeys.value.pop()
+  if (lastLetter === ' ' && lastLetter === event.key) {
+    try {
+      await store.fetchCorrectAnswer()
+      showAnswer.value = true
+      previousKeys.value.pop()
+    }
+    catch (error) {
+      /* TOOD: some modals */
+      console.error(error)
+    }
     return
   }
 
@@ -106,106 +112,126 @@ const keyup = async (event: KeyboardEvent) => {
   // if we type really fast this can fail to enter
   // character at the end
   // change to a function that finds the last occurance of character and replaces that
-  if (commandBefore === ',') {
+  if (lastLetter === ',') {
     const indexOfLastChar = input.value.lastIndexOf(event.key)
-    const lastChar = input.value[indexOfLastChar]
 
     if (event.key === 'a') {
       input.value = input.value.substring(0, indexOfLastChar) + specialLatinLetters[0] + input.value.substring(indexOfLastChar + 1)
     }
-
-    if (event.key === 'o') {
+    else if (event.key === 'o') {
       input.value = input.value.substring(0, indexOfLastChar) + specialLatinLetters[1] + input.value.substring(indexOfLastChar + 1)
     }
-
-    if (event.key === 'i') {
+    else if (event.key === 'i') {
       input.value = input.value.substring(0, indexOfLastChar) + specialLatinLetters[2] + input.value.substring(indexOfLastChar + 1)
     }
-
-    if (event.key === 'e') {
+    else if (event.key === 'e') {
       input.value = input.value.substring(0, indexOfLastChar) + specialLatinLetters[3] + input.value.substring(indexOfLastChar + 1)
     }
-
-    if (event.key === 'u') {
+    else if (event.key === 'u') {
       input.value = input.value.substring(0, indexOfLastChar) + specialLatinLetters[4] + input.value.substring(indexOfLastChar + 1)
     }
   }
 }
+
+const sanitizeAnswerWithLatin = (answer: string): string => {
+  return answer
+    .replace(/ā/g, 'a')
+    .replace(/ō/g, 'o')
+    .replace(/ī/g, 'i')
+    .replace(/ē/g, 'e')
+    .replace(/ū/g, 'u')
+}
+
+const questionAnswerParsed = computed(() => {
+  return hasEasyModeEnabled.value
+    ? sanitizeAnswerWithLatin(questionAnswer.value ?? '')
+    : questionAnswer.value
+})
 </script>
 
 <template>
   <BaseCard
     class="p-20 relative min-w-full max-w-8xl"
-    :class="{ shake: warningAnimation }"
+    :class="{ 'shake': warningAnimation, 'pb-28': showAnswer }"
     @keyup="keyup"
   >
-    <div class="text-center">
+    <div class="text-center relative">
       <div
         class="mb-4 max-w-xl mx-auto text-center rounded-md"
         :class="{ 'correct-glow': correctAnimation }"
       >
         {{ currentExercise?.question }}
       </div>
-      <p
-        v-if="showAnswer"
-        class="my-4"
+      <Transition
+        name="answer"
+        mode="out-in"
       >
-        {{ questionAnswer }}
-      </p>
-      <form
-        class="grid gap-4 mb-4"
-        @submit.prevent="submit"
-      >
-        <div
-          v-show="!hasEasyModeEnabled"
-          class="flex mx-auto gap-4"
+        <p
+          v-if="showAnswer"
+          class="absolute flex justify-center w-full"
         >
-          <BaseButton
-            v-for="letter in specialLatinLetters"
-            :key="letter"
-            perma-shadow
-            class="min-w-20"
-            type="button"
-            @click="input += letter"
-          >
-            {{ letter }}
-          </BaseButton>
-        </div>
-        <BaseInput
-          ref="inputRef"
-          name="answer"
-          class="mx-auto max-w-[450px]"
-          :input="input"
-          placeholder="enter text here"
-          autofocus
-          @keydown="preventSpace"
-          @update:input="onInput"
-        />
-        <BaseButton
-          :class="{ 'correct-glow': correctAnimation }"
-          class="mx-auto w-[450px]"
-        >
-          Check
-        </BaseButton>
-      </form>
-      <div>
-        <div
-          v-show="!hasEasyModeEnabled"
-          class="text-lg mb-4"
-        >
-          Use
-          <BaseKey letter="," /> +
-          <BaseKey letter="a" />
-          <BaseKey letter="i" />
-          <BaseKey letter="o" />
-          <BaseKey letter="u" />
-          to automatically enter a character
-        </div>
-        <p class="text-lg">
-          Press
-          <BaseKey letter="space" /> twice to show the
-          answer
+          {{ questionAnswerParsed }}
         </p>
+      </Transition>
+      <div
+        class="neighbor"
+        :class="{ animate: showAnswer }"
+      >
+        <form
+          class=" grid gap-4 mb-4 transition-all duration-300"
+          @submit.prevent="submit"
+        >
+          <div
+            v-show="!hasEasyModeEnabled"
+            class="flex mx-auto gap-4"
+          >
+            <BaseButton
+              v-for="letter in specialLatinLetters"
+              :key="letter"
+              perma-shadow
+              class="min-w-20"
+              type="button"
+              @click="input += letter"
+            >
+              {{ letter }}
+            </BaseButton>
+          </div>
+          <BaseInput
+            ref="inputRef"
+            name="answer"
+            class=" mx-auto max-w-[450px]"
+            :input="input"
+            placeholder="enter text here"
+            autofocus
+            @keydown="preventSpace"
+            @update:input="onInput"
+          />
+          <BaseButton
+            :class="{ 'correct-glow': correctAnimation }"
+            class="mx-auto w-[450px]"
+          >
+            Check
+          </BaseButton>
+        </form>
+        <div>
+          <div
+            v-show="!hasEasyModeEnabled"
+            class="text-lg mb-4"
+          >
+            Use
+            <BaseKey letter="," /> +
+            <BaseKey letter="a" />
+            <BaseKey letter="i" />
+            <BaseKey letter="o" />
+            <BaseKey letter="u" />
+            to automatically enter a character
+          </div>
+          <p class="text-lg">
+            Press
+            <BaseKey letter="space" /> twice to show the
+            answer
+          </p>
+        </div>
       </div>
     </div>
     <div class="mx-auto absolute -right-80 top-0 max-w-[300px]">
@@ -223,9 +249,7 @@ const keyup = async (event: KeyboardEvent) => {
         <span class="line-clamp-8">
           Easy mode
         </span>
-        <CheckIcon
-          class="w-[24px] h-[24px] text-black font-bold bg-brand dark:bg-brand rounded-sm"
-        />
+        <CheckIcon class="w-[24px] h-[24px] text-black font-bold bg-brand dark:bg-brand rounded-sm" />
       </div>
     </div>
   </BaseCard>
@@ -298,5 +322,33 @@ const keyup = async (event: KeyboardEvent) => {
 .v-enter-from,
 .v-leave-to {
   opacity: 0;
+}
+
+.answer-enter-active {
+  transition: opacity 0.5s ease;
+  transition: all 0.3s ease-out;
+}
+
+.answer-leave-active {
+  transition: all 0.2s cubic-bezier(1, 0.5, 0.8, 1);
+  position: absolute;
+}
+
+.answer-enter-from,
+.answer-leave-to {
+  transform: translateY(10px);
+  opacity: 0;
+}
+
+.neighbor {
+  transition: transform 0.8s ease;
+  /* Define the transition */
+  transform: translateY(0);
+  /* Define the initial state */
+}
+
+.neighbor.animate {
+  transform: translateY(50px);
+  /* Or any other transform */
 }
 </style>
